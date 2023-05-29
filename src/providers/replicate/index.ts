08d6afdbad076f91c9426a74a5bcdbfc4abb6d54
satriacloudx <133637951+satriacloudx@users.noreply.github.com>
2023-05-29 10:19:27 +0700

    Update index.ts

--- src/providers/replicate/index.ts
+++ src/providers/replicate/index.ts
@@ -8,7 +8,7 @@ const providerReplicate = () => {
     name: 'Replicate',
     globalSettings: [
       {
-        key: 'r8_I90bxKGZd7EKpjW9d8S6R6TnoPaLGVe1gMV93',
+        key: 'token',
         name: 'Replicate API token',
         type: 'api-key',
       },
